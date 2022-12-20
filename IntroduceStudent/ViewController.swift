380306df2cb9f4f09f627f9d549effe323fd78fd
Juan Ceruli <77794076+cerulij@users.noreply.github.com>
2022-12-20 10:50:12 -0300

    Added degree type to layout

--- IntroduceStudent/ViewController.swift
+++ IntroduceStudent/ViewController.swift
@@ -9,28 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var morePetsStepper: UIStepper!
-    
-    @IBOutlet weak var morePetsSwitch: UISwitch!
-    
     @IBOutlet weak var firstNameTextField: UITextField!
 
     @IBOutlet weak var lastNameTextField: UITextField!
     
     @IBOutlet weak var schoolNameTextField: UITextField!
     
-    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var degreeSegmentedControl: UISegmentedControl!
     
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
     
     @IBAction func stepperDidChange(_ sender: UIStepper) {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
     
     @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let degree = degreeSegmentedControl.titleForSegment(at:
+            degreeSegmentedControl.selectedSegmentIndex)
+        
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
         
-        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year of my \(degree!) degree and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
         
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         
